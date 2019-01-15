0b848dd6d9d1d669272556a24d3c5c65bd59bbb7
AlexL-Productions <39320624+AlexL-Productions@users.noreply.github.com>
2019-01-15 14:18:58 -0500

    First upload C++

---
+++ M1T1_LopezAlex/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+// CSC 134
+// M1T1
+// Alex Lopez
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello from CSC134!" << endl;
+    cout << "THis is some more text"<< endl;
+    int num = (10 * 10) + 50 / 2;
+    cout << num << endl;
+    return 0;
+}
