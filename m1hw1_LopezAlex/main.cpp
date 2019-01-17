5cec204e0f673e72699f0b7500486acdf260cc4e
AlexL-Productions <39320624+AlexL-Productions@users.noreply.github.com>
2019-01-17 14:07:39 -0500

    Add files via upload

---
+++ m1hw1_LopezAlex/main.cpp
@@ -0,0 +1,29 @@
+// CSC 134
+// M1HW1
+// Alex Lopez
+// 1/17/2019
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // string variable to hold programming major
+    string major = "Computer Science";
+
+    // int variable for year
+    int year = 2017;
+
+    // int variable for graduation date
+    int gradYear = 2019;
+
+    // display introduction to screen
+    cout << "Hello my name is Alex Lopez!" << endl;
+    cout << "I'm majoring in:\t\t" << major << endl;
+    cout << "I started programming in:\t"<< year << endl;
+    cout << "I will graduate from FTCC in:\t" << gradYear << endl;
+    cout << "I have " << 5 << " dogs."<< endl;
+    cout << endl;
+    return 0;
+}
